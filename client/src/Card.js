af7e7942ed477a701c78a56ec7948e271b0fcfd0
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-03-07 13:23:32 +0100

    Initial commit

---
+++ client/src/Card.js
@@ -0,0 +1,57 @@
+class Card{
+
+    constructor(xPos, yPos, frontImg, backImg, frameImg, cardName, categoryId){
+
+        this.xPos       = xPos;
+        this.yPos       = yPos;
+        this.frontImg   = frontImg;
+        this.backImg    = backImg;
+        this.frameImg   = frameImg;
+        this.cardName   = cardName;
+        this.categoryId = categoryId;
+
+    }
+}
+
+class UnitCard extends Card{
+
+    constructor(value, description, effect, rarity, type){
+        super(xPos, yPos, frontImg, backImg, frameImg, cardName, categoryId, type);
+        this.value          = value;
+        this.description    = description;
+        this.effect         = effect;
+        this.rarity         = rarity;
+        this.type           = type;
+
+    }
+}
+
+class SuddenCard extends Card {
+
+    constructor(description, effect, type){
+        super(xPos, yPos, frontImg, backImg, frameImg, cardName, categoryId, type);
+        this.description    = description;
+        this.effect         = effect;
+        this.type           = type;
+    }
+}
+
+class PermaCard extends Card{
+
+    constructor(description, effect, type){
+        super(xPos, yPos, frontImg, backImg, frameImg, cardName, categoryId, type);
+        this.description    = description;
+        this.effect         = effect;
+        this.type           = type;
+    }
+}
+
+
+class ClimateCard extends Card{
+
+    constructor (effect, type){
+        super(xPos, yPos, frontImg, backImg, frameImg, cardName, categoryId, type);
+        this.effect     = effect;
+        this.type       = type;
+    }
+}
