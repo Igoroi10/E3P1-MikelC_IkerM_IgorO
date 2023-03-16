34ce46ebe3d4851c094fdc47a0e1bb5a59f4769d
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-03-16 10:45:36 +0100

    [UPDATE] Render cards subido de nuevo

--- client/src/gameRender.js
+++ client/src/gameRender.js
@@ -1,4 +1,5 @@
 import globals from "./globals.js";
+import {CardSizes, CardCategory} from "./constants.js";
 
 
 
@@ -12,6 +13,7 @@ export default function render()
 function drawGame(){
     renderMap();
     renderCards();
+    renderBigCard();
 
 }
 
@@ -30,17 +32,135 @@ function renderCards(){
 }
 
 function renderCard(card){
+
+     //DIBUJO DEL REVERSO
     if(card.showBack === true){
         globals.ctx.drawImage(
-            globals.assets.reverse,                     //archivo de la imagen
-            card.xPos, card.yPos,                       //Posición inicial x e y 
-            card.imageSet.xSize, sprite.imageSet.ySize, 
-            card.xPos, card.yPos,                        //fin de x e y
-            card.imageSet.xSize, card.imageSet.ySize    //Fin de anchura.       
+            globals.tileSets.card_reverse[cards[i].reverse],                     //archivo de la imagen
+            card.xPos, card.yPos,                                   //Posición inicial x e y 
+            card.imageSet.xSize, card.imageSet.ySize, 
+            card.xPos, card.yPos,                                   //fin de x e y
+            CardSizes.TOKEN_WIDHT, CardSizes.TOKEN_HEIGHT             //Fin de anchura.       
 
         )
 
     }
+
+    //DIBUJO DEL ANVERSO DE LA UNIDAD 
+    else if (card.category === CardCategory.UNIT){
+
+        //Imagen de fondo
+        globals.ctx.drawImage(
+            globals.tileSets.card_reverse[cards[i].reverse],                     //archivo de la imagen
+            card.xPos, card.yPos,                                   //Posición inicial x e y 
+            card.imageSet.xSize, card.imageSet.ySize, 
+            card.xPos, card.yPos,                                   //fin de x e y
+            CardSizes.SMALL_WIDTH, CardSizes.SMALL_HEIGHT             //Fin de anchura.       
+
+        )
+        //Frame
+        globals.ctx.drawImage(
+            globals.tileSets.card_front_img[cards[i].frontImg],                     //archivo de la imagen
+            card.xPos, card.yPos,                                   //Posición inicial x e y 
+            card.imageSet.xSize, card.imageSet.ySize, 
+            card.xPos, card.yPos,                                   //fin de x e y
+            CardSizes.SMALL_WIDTH, CardSizes.SMALL_HEIGHT             //Fin de anchura.       
+
+        )
+        //Icono superior izquierda
+        globals.ctx.drawImage(
+            globals.tileSets.card_reverse[cards[i].type],                     //archivo de la imagen
+            card.xPos, card.yPos,                                   //Posición inicial x e y 
+            card.imageSet.xSize, card.imageSet.ySize, 
+            card.xPos, card.yPos,                                   //fin de x e y
+            card.imageSet.xSize, card.imageSet.ySize             //Fin de anchura.       
+
+        )
+        //Icono centro izquierda
+        globals.ctx.drawImage(
+            globals.tileSets.card_reverse[cards[i].value],                     //archivo de la imagen
+            card.xPos, card.yPos,                                   //Posición inicial x e y 
+            card.imageSet.xSize, card.imageSet.ySize, 
+            card.xPos, card.yPos,                                   //fin de x e y
+            card.imageSet.xSize, card.imageSet.ySize             //Fin de anchura.       
+
+        )
+
+        //Icono abajo izquierda
+        globals.ctx.drawImage(
+            globals.tileSets.card_reverse[cards[i].reverse],                     //archivo de la imagen
+            card.xPos, card.yPos,                                   //Posición inicial x e y 
+            card.imageSet.xSize, card.imageSet.ySize, 
+            card.xPos, card.yPos,                                   //fin de x e y
+            card.imageSet.xSize, card.imageSet.ySize             //Fin de anchura.       
+
+        )
+
+    }
+    //DIBUJO DEL ANVERSO DEL RESTO DE CARTAS EXCEPTO TOKEN
+    else if(card.category !== CardCategory.TOKEN){
+        //Imagen de fondo
+        globals.ctx.drawImage(
+            globals.tileSets.card_reverse[cards[i].reverse],                     //archivo de la imagen
+            card.xPos, card.yPos,                                   //Posición inicial x e y 
+            card.imageSet.xSize, card.imageSet.ySize, 
+            card.xPos, card.yPos,                                   //fin de x e y
+            CardSizes.SMALL_WIDTH, CardSizes.SMALL_HEIGHT             //Fin de anchura.       
+
+        )
+        //Frame
+        globals.ctx.drawImage(
+            globals.tileSets.card_front_img[cards[i].frontImg],                     //archivo de la imagen
+            card.xPos, card.yPos,                                   //Posición inicial x e y 
+            card.imageSet.xSize, card.imageSet.ySize, 
+            card.xPos, card.yPos,                                   //fin de x e y
+            CardSizes.SMALL_WIDTH, CardSizes.SMALL_HEIGHT             //Fin de anchura.       
+
+        )
+        //Icono superior izquierda
+        globals.ctx.drawImage(
+            globals.tileSets.card_reverse[cards[i].type],                     //archivo de la imagen
+            card.xPos, card.yPos,                                   //Posición inicial x e y 
+            card.imageSet.xSize, card.imageSet.ySize, 
+            card.xPos, card.yPos,                                   //fin de x e y
+            card.imageSet.xSize, card.imageSet.ySize             //Fin de anchura.       
+
+        )
+        //Icono abajo izquierda
+        globals.ctx.drawImage(
+            globals.tileSets.card_reverse[cards[i].value],                     //archivo de la imagen
+            card.xPos, card.yPos,                                   //Posición inicial x e y 
+            card.imageSet.xSize, card.imageSet.ySize, 
+            card.xPos, card.yPos,                                   //fin de x e y
+            card.imageSet.xSize, card.imageSet.ySize             //Fin de anchura.       
+
+        )
+    }
+    //DIBUJO ANVERSO DEL  TOKEN
+    else{
+        //Imagen de fondo
+        globals.ctx.drawImage(
+            globals.tileSets.card_reverse[cards[i].reverse],                     //archivo de la imagen
+            card.xPos, card.yPos,                                   //Posición inicial x e y 
+            card.imageSet.xSize, card.imageSet.ySize, 
+            card.xPos, card.yPos,                                   //fin de x e y
+            CardSizes.TOKEN_WIDHT, CardSizes.TOKEN_HEIGHT             //Fin de anchura.       
+
+        )
+        //Frame
+        globals.ctx.drawImage(
+            globals.tileSets.card_front_img[cards[i].frontImg],                     //archivo de la imagen
+            card.xPos, card.yPos,                                   //Posición inicial x e y 
+            card.imageSet.xSize, card.imageSet.ySize, 
+            card.xPos, card.yPos,                                   //fin de x e y
+            CardSizes.TOKEN_WIDHT, CardSizes.TOKEN_HEIGHT             //Fin de anchura.       
+
+        )
+    }
+}
+
+function renderBigCard(){
+
 }
 
 export{
