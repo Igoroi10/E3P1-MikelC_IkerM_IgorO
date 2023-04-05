67210bd96a07ef1b8eec49b7ba6fa31b79311e68
Igoroi10 <igor.ocon@ikasle.aeg.eus>
2023-04-05 09:45:06 +0200

    [NEW] shuffleDeck create in order to shuffle the deck

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -369,6 +369,25 @@ function startingDeal(mode){
 
 }
 
+function shuffleDeck(deck){
+
+    let currentIndex = deck.length;
+    let randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+}
+
 export {
     update,
     checkStates,
