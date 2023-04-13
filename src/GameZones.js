c9fbf2add9705e80f06ccf09bd20bc0cdb69b887
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2023-04-13 16:15:33 +0200

    [FIX] Correccion de nombre de slotIdentificator

--- src/GameZones.js
+++ src/GameZones.js
@@ -1,13 +1,13 @@
 class GameZones {
 
-    constructor (xPos, yPos, xSize, ySize, slotIDentificator)
+    constructor (xPos, yPos, xSize, ySize, slotIdentificator)
     {
         this.xPos               = xPos;
         this.yPos               = yPos;
         this.xSize              = xSize;
         this.ySize              = ySize;
         this.placed_cards       = 0;    //Si existe una carta tendra el numero correspopndiente de la carta, en caso de no haber ninguna carta tendra -1
-        this.slotIDentificator  = slotIDentificator;
+        this.slotIdentificator  = slotIdentificator;
     }
 
 }
