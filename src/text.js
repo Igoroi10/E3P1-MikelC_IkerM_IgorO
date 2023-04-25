9299abd3546b05195d04c185a489808b24afd662
IKER MENDOZA LABORRA <iker.mendoza@ikasle.aeg.eus>
2023-04-25 09:23:32 +0200

    [NEW] bi array sortu testuekin

---
+++ src/text.js
@@ -0,0 +1,14 @@
+import globals from "./globals"
+
+const lenguajeText = [
+    [emailText = 'Email', passwordText = 'Password', winrateText = 'Winrate', gamesWonText = 'Games won', totalGamesText = 'Total games', userListText = 'Users List', selectDifficultText = 'Select game difficult:', normalButtonText = 'Normal', expertButtonText = 'Expert', logOutText = 'Log out'],
+    [emailText = 'Emaila', passwordText = 'Pasahitza', winrateText = 'Irabazi tasa', gamesWonText = 'Irabazitako partidak', totalGamesText = 'Jokatutako partidak', userListText = 'Erabiltzaile zerrenda', selectDifficultText = 'Partidaren zailtasuna aukeratu:', normalButtonText = 'Normala', expertButtonText = 'Aditua', logOutText = 'Sahioa itxi'],
+]
+
+const gameText = [
+    [turnText = "'s turn", selectCardText = 'Select a card and put it on the table', decoyText = '(Select a card to decoy)', onePlayerPassedText = 'Only player ' + globals.turnState + ' can play'],
+    [turnText = "-ren txanda", selectCardText = 'Karta bat aukeratu eta mahaian ipini', decoyText = '(Karta bat aukeratu decoy egiteko)', onePlayerPassedText = 'Bakarrik ' + globals.turnState + ' jokalariak jolastu dezake']
+]
+
+
+    
